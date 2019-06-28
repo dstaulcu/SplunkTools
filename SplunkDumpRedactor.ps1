267e43e467f7f99f17709d8d2df6ed5481e5ad46
dstaulcu <dstaulcu@users.noreply.github.com>
2019-06-27 22:32:28 -0400

    Add files via upload

---
+++ SplunkDumpRedactor.ps1
@@ -0,0 +1,27 @@
+﻿$file = "C:\dump.txt"
+$content = Get-Content -Path $file
+
+$newfile = $file -replace "\.","_masked."
+if (Test-Path -Path $newfile) { Remove-Item $newfile -Force }
+
+$newcontent = @()
+foreach ($line in $content) {
+    if ($line -imatch "(DC=|CN=|\[|bindDNpassword = )") {
+        $newline = $line
+        $newline = $newline -replace "DC=[^,]+","DC=XXXXXX"
+        $newline = $newline -replace "CN=[^,]+","CN=XXXXXX"
+        $newline = $newline -replace "\[\S+\]+","[XXXXXX]"
+        $newline = $newline -replace "bindDNpassword = .*","bindDNpassword = XXXXX"
+
+        write-host "`noldline: $($line)"
+        write-host "newline: $($newline)"
+
+        $newcontent += $newline
+
+    } else {
+        $newcontent += $line
+    }
+}
+
+$newcontent | Add-Content -Path $newfile
+write-host "Masked content written $($newfile)."
