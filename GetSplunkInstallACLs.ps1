55c90cbf7856f8a2bcdda6c5b580721b5994f1a8
dstaulcu <dstaulcu@users.noreply.github.com>
2020-01-14 23:11:53 -0500

    Add files via upload

---
+++ GetSplunkInstallACLs.ps1
@@ -0,0 +1,49 @@
+﻿$SplunkInstallDir = "C:\Program Files\SplunkUniversalForwarder"
+
+# get the version of Splunk
+$SplunkReadmeFile = Get-ChildItem -Path $SplunkInstallDir -Filter *README*.txt
+$SplunkVersion = $SplunkReadmeFile | Get-Content | select-string -Pattern "^Splunk \d+"
+
+
+$DataSet = @()
+
+$folders = Get-ChildItem -Path $SplunkInstallDir -Directory -Recurse
+
+foreach ($folder in $folders) {
+
+
+    $ACL = Get-Acl -Path $folder.FullName 
+
+    foreach ($ACE in $ACL.Access) {
+
+
+        if (($ACE.IdentityReference -match "(Everyone|Users)") -and ($ACE.FileSystemRights -notmatch "\d+")) {
+
+#            write-host $folder.fullname
+#            write-host $ACE.IdentityReference
+
+            $info = @{
+                "SplunkVersion" = $SplunkVersion
+                "FolderName" = $folder.FullName
+                "IdentityReference" = $ACE.IdentityReference
+                "FileSystemRights" = $ACE.FileSystemRights
+            }
+
+            $DataSet += New-Object -TypeName PSObject -Property $info
+
+        }
+
+
+    }
+
+}
+
+$SplunkVersion = $SplunkVersion -replace " ","_"
+$DataSet | Export-Csv -Path "C:\Development\ACLList_$($SplunkVersion).txt" -NoTypeInformation
+$Dataset | Out-GridView
+
+
+
+Get-ChildItem -Path C:\Development -Filter ACLList*.txt
+
+
