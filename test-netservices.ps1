30adde7da1544b30c5e7f8c3cb569435fc9da715
dstaulcu <dstaulcu@users.noreply.github.com>
2020-03-15 11:24:22 -0400

    Update test-netservices.ps1

--- test-netservices.ps1
+++ test-netservices.ps1
@@ -1,8 +1,13 @@
+# tcp services to be tested
 $services = @(
     "192.168.1.1:80:Home Router Web Interface",
     "192.168.1.1:81:Home Router Web Interface2"
     "127.0.0.1:8089:Local host Splunk admin port"    
 )
+
+# initialize output file
+$outputFile = "$($env:temp)\test_connection_results.csv"
+if (test-path -path $outputFile) { Remove-Item -Path $outputFile -Force}
 
 function test-service {
     param ($ipaddress, $port)
@@ -20,22 +25,30 @@ function test-service {
 
 }
 
+# initialize array for results
 $Results = @()
-foreach ($service in $services) {
 
+# test each of the services
+foreach ($service in $services) {
     $Server = ($service -split ":")[0].trim()
     $Port = ($service -split ":")[1].trim()
     $Description = ($service -split ":")[2].trim()
 
+    $test_time = Get-Date
     $info = @{
         Server = $Server
         Port = $Port
         Description = $Description
-        ConnectionSucceeded = test-service -ipaddress $server -port $Port
+        TestStatus = test-service -ipaddress $server -port $Port
+        TestDurationMs = (New-TimeSpan -Start $test_time).TotalMilliseconds
     }
 
-    $Results += New-Object -TypeName PSObject -Property $Info
-    
+    $Results += New-Object -TypeName PSObject -Property $Info    
 }
 
-$Results | Select-Object -Property Server, Port, Description, ConnectionSucceeded | Sort-Object -Property Server, Port | Out-GridView -Title "Test Results"
+# write results to results file
+$Results | Export-Csv -NoTypeInformation -Path $outputFile
+write-host "Results written to output file: $($outputfile)."
+
+$outputFile | Import-Csv | select-object -property Server, Port, Description, TestStatus, TestDurationMs | Sort-Object Server, Port | Out-GridView -Title "Test results in output file $($outputfile)"
+
