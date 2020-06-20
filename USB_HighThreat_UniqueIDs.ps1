2cb966b28a2bff3c33704f2f878eadc43d2823eb
dstaulcu <dstaulcu@users.noreply.github.com>
2020-06-20 14:47:25 -0400

    Update USB_HighThreat_UniqueIDs.ps1

--- USB_HighThreat_UniqueIDs.ps1
+++ USB_HighThreat_UniqueIDs.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 https://www.silabs.com/community/interface/knowledge-base.entry.html/2013/11/21/windows_usb_devicep-aGxD
 
 Composite USB Device Path Format For Interfaces
@@ -18,13 +18,6 @@ gggggggg-gggg-gggg-gggg-gggggggggggg is the device interface GUID that is used t
 # this technique only works for usbstuff
 $PnpDevices = Get-PnpDevice | ?{$_.Instanceid -match "^USB"}
 
-<#
-# further reduce list down to device classes which are scary exfil channels
-- WPD=Phones, DiskDrive=ThumbDrives, Media=Micrphones(sometimes), Image=WebCams(sometimes), WebCam=WebCams(sometimes), BlueTooth=duh)
-#>
-
-$PnpDevices = $PnpDevices | ?{$_.class -match "^(WPD|BLUETOOTH|DiskDrive|Media|Image|WebCam)"}
-
 $Records = @()
 
 foreach ($PnpDevice in $PnpDevices) {
@@ -33,28 +26,71 @@ foreach ($PnpDevice in $PnpDevices) {
 
     # Unique ID (aka "serial" appears to be a token inside of device parent key
     $UniqueID = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_Parent"}).Data
-    # isolate chars to right of last backslash
-    $UniqueID = ($UniqueID -split "\\")[2]
-    # isolate chars between first and second amperstand instance, if exist
-    if ($UniqueID -match "&") { $UniqueID = ($UniqueID -split "&")[1] }
+
+    $MatchedPattern = $false
+
+    if ($UniqueID) {
+
+        # isolate chars to right of second \, if present
+        if (($UniqueID -split "\\").count -eq 3) { 
+
+            $UniqueID = ($UniqueID -split "\\")[2]
+            
+            if ($UniqueID -match "&") { 
+                # use second token after "&" delimeter, if present
+                $MatchedPattern = $true
+                $UniqueID = ($UniqueID -split "&")[1] 
+            } else {
+                if ($UniqueID.Length -ne "0") { $MatchedPattern = $true }
+            }
+        }
+    }
+    
+    # if the property did not have the expected structure, report N/A
+    if ($MatchedPattern -eq $false ) { $UniqueID = "n/a" }
+
+    $FirstInstallDate = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_FirstInstallDate"}).Data
+    $LastArrivalDate = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_LastArrivalDate"}).Data
+    $Category = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_DeviceContainer_Category"}).Data
+    $IsPresent =($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_IsPresent"}).Data
+    $Parent = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_Parent"}).Data
+
+    if ($PnpDevice.Class -match "^(WPD|BLUETOOTH|DiskDrive|Media|Image|WebCam)$") {
+        $RiskLevel = "High"
+    } else { 
+        $RiskLevel = "Undefined"
+    }
 
     $Record  = @{
-        Device_Class = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_Class"}).Data
-        Device_Manufacturer = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_Manufacturer"}).Data
-        Device_FriendlyName = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_FriendlyName"}).Data
-        Device_FirstInstallDate = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_FirstInstallDate"}).Data
-        Device_LastArrivalDate = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_LastArrivalDate"}).Data
-        DeviceContainer_Category = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_DeviceContainer_Category"}).Data
-        DEVPKEY_Device_IsPresent =($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_IsPresent"}).Data
-        DEVPKEY_Device_Parent = ($PnpDeviceProperty | ? {$_.KeyName -eq "DEVPKEY_Device_Parent"}).Data
+
+        FirstInstallDate = $FirstInstallDate
+        LastArrivalDate = $LastArrivalDate
+        Category = $Category
+        IsPresent = $IsPresent
+        Parent = $Parent
 
         UniqueID = $UniqueID        
-        PnpDeviceClass = $PnpDevice.Class
-        PnpDeviceService = $PnpDevice.Service
-        PnpDeviceStatus = $PnpDevice.Status
+        Class = $PnpDevice.Class
+        Service = $PnpDevice.Service
+        Status = $PnpDevice.Status
+        FriendlyName = $PnpDevice.FriendlyName
+        InstanceID = $PnpDevice.InstanceId
+
+        RiskLevel = $RiskLevel
+        
+
     }
+
 
     $Records += New-Object -TypeName PSObject -Property $Record
 }
 
-$Records | Out-GridView
+<#
+# Reduce list down to devices which are present
+$Records = $Records | ?{$_.IsPresent -eq "True"}
+
+# Reduce list down to devices which are high risk
+$Records = $Records | ?{$_.RiskLevel -eq "High"}
+#>
+
+$Records | Select InstanceID, Class, Service, Category, RiskLevel, FriendlyName, UniqueID, Status, IsPresent, LastArrivalDate, FirstInstallDate | Sort-Object -Property LastArrivalDate -Descending | Out-GridView
