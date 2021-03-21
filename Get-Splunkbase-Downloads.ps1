1bb65db7655e9de133dd8133a500602d87109303
dstaulcu <dstaulcu@users.noreply.github.com>
2021-03-20 23:13:30 -0400

    Update Get-Splunkbase-Downloads.ps1

--- Get-Splunkbase-Downloads.ps1
+++ Get-Splunkbase-Downloads.ps1
@@ -1,43 +1,86 @@
-﻿<#
-.SYNOPSIS
-	Download selected apps from splunkbase in either interactive or unattended mode
-.PARAMETER app_id_list
-	download filter based on comma separated list of ids of apps. (all by default)
-.PARAMETER app_age_days
-	download filter based on number of days since last update. (7 days by default)
-.PARAMETER download_path
-	path to folder where download files are saved. (%temp% by default)
-.PARAMETER cred_file_path
-	path to file having credentials stored as secure string (prompt for credentials by default)
-.PARAMETER unattended
-	boolean ($true or $false) denoting whether script should run unattended. cred_file_path parameter required.
-.NOTES
-    check for updates here: https://github.com/dstaulcu/SplunkTools	
-.EXAMPLE
-    .\get-splunkbase-downloads.ps1 -download_path C:\Apps\Splunkbase
-.EXAMPLE
-    .\get-splunkbase-downloads.ps1 -download_path C:\Apps\Splunkbase -app_age_days 7 -cred_file_path C:\Apps\Splunkbase\support\mycred.xml -app_id_list "1914,742" -unnatended $true
+<#
+$DebugPreference = "Continue"
 #>
 
+$ProjectFolder = "C:\Apps\splunkbase"
+$SupportFolder = "$($ProjectFolder)\support"
+$AppsToTrack = "$($SupportFolder)\myapps.csv"
+$BookmarkFile = "$($SupportFolder)\LastModifiedDate.md"
+$CatalogFile =  "$($ProjectFolder)\catalog.csv"
 
-param([string]$download_path="$($env:temp)"
-    ,[int]$app_age_days="7"
-    ,[string]$app_id_list=$null
-    ,[string]$cred_file_path=$null
-    ,[boolean]$unattended=$false
-)
+# if catalog file is missing, start anew
+if (-not(test-path -path $CatalogFile)) {
+    if (Test-Path -Path $BookmarkFile) { remove-item -Path $BookmarkFile }
+}
+# if bookmark file exists, get the date it contains
+if (Test-Path -Path $BookmarkFile) {
+    [datetime]$BookmarkDate = Get-Content -Path $BookmarkFile
+} else {
+    $BookmarkDate =  Get-Date -Date "01/01/1970"
+}
 
-<##############################################################################
-FUNCTIONS 
-##############################################################################>
 
-## with input of appid, query splunkbase for version number and file name and return in appinfo object.
+function Get-Splunkbase-SessionCookie {
+    param($credFilePath)
+    $cred = Import-Clixml $credFilePath
+    $user = $cred.UserName
+    $pass = [System.Net.NetworkCredential]::new("", $cred.Password).Password
+
+    ## establish logon session to splunk via okta
+    $BASE_AUTH_URL='https://account.splunk.com/api/v1/okta/auth'
+    $Body = @{
+        username = $user
+        password = $pass
+    }
+    $WebRequest = Invoke-WebRequest $BASE_AUTH_URL -SessionVariable 'Session' -Body $Body -Method 'POST' -UseBasicParsing
+    if (!($WebRequest.StatusCode -eq "200")) {
+        write-host "There was a problem authenticating to Splunk.  Exit."
+        exit
+    }
+    $ssoid_cookie = (($WebRequest.Content | ConvertFrom-Json).cookies).ssoid_cookie
+    $sid = ($Session.Cookies.GetCookies("https://splunkbase.splunk.com") | ?{$_.Name -eq "sid"}).Value
+
+    $Cookie = "sid=$($sid); SSOSID=$($ssoid_cookie)"
+
+    return $Cookie
+}
+
+function Get-Splunkbase-Session {
+    param($credFilePath)
+    $cred = Import-Clixml $credFilePath
+    $user = $cred.UserName
+    $pass = [System.Net.NetworkCredential]::new("", $cred.Password).Password
+
+    ## establish logon session to splunk via okta
+    $BASE_AUTH_URL='https://account.splunk.com/api/v1/okta/auth'
+    $Body = @{
+        username = $user
+        password = $pass
+    }
+    $WebRequest = Invoke-WebRequest $BASE_AUTH_URL -SessionVariable 'Session' -Body $Body -Method 'POST' -UseBasicParsing
+    if (!($WebRequest.StatusCode -eq "200")) {
+        write-host "There was a problem authenticating to Splunk.  Exit."
+        exit
+    }
+
+    $ssoid_cookie = (($WebRequest.Content | ConvertFrom-Json).cookies).ssoid_cookie
+
+    $cookie = New-Object System.Net.Cookie    
+    $cookie.Name = "SSOSID"
+    $cookie.Value = $ssoid_cookie
+    $cookie.Domain = ".splunk.com"
+    $session.Cookies.Add($cookie);
+
+    return $session
+}
+
 function Get-Splunkbase-AppInfo {
-    param($AppID,$session)
+    param($AppID=4023,$session=$null)
 
-    $WebRequest = Invoke-WebRequest -Uri "https://splunkbase.splunk.com/app/$($AppID)/" -WebSession $session
+    $WebRequest = Invoke-WebRequest -Uri "https://splunkbase.splunk.com/app/$($appid)/" -WebSession $session
 
-    # Find the DIV element of expected class having release-version info
+    # EXTRACT OUT THE APP VERSION
+
     $element = $WebRequest.AllElements | ?{
         $_.class -eq "u.item:1/1@*" -and 
         $_.tagname -eq "DIV" -and 
@@ -45,176 +88,168 @@ function Get-Splunkbase-AppInfo {
         $_.innerhtml -match "u-for=`"download-modal`"" -and
         $_.innerhtml -match "checksum" -and
         $_.outertext -match "^Downloading"}
+    
 
     # extract out the release version (sb-target value)
-    $element.innerHTML -match "sb-target=`"([^\`"]+)`"" | Out-Null
-    $version = $matches[1]
+    $version = "unknown"
+    $filename = "unknown"
+    $m = $element.innerHTML | select-string -pattern 'sb-target=\"([^\"]+)\".*?checksum \(([^\)]+)\)' -AllMatches
+    if ($m) { 
+        $version = $m.matches.captures[0].Groups[1].value 
+        $filename = $m.matches.captures[0].Groups[2].value
+    } else {
+        $version = "none"
+        $filename = "none"
+    }
 
-    # extract out the download filename
-    $element.innerHTML -match " checksum \(([^\)]+)" | Out-Null
-    $filename = $matches[1]
+    # EXTRACT OUT THE SPLUNK VERSIONS SUPPORTED BY THIS VERSION OF APP
+       
+    # get all container info
+    $element = $WebRequest.AllElements | ?{
+        $_.class -eq "u.container:vspace-xs@* u.container:vpad-md" -and
+        $_.tagname -eq "DIV" -and 
+        $_.innerhtml -match "/apps/#/version/"
+    }
 
-    # return new object with desired info
-    [PSCustomObject]@{
-        filename=$filename
-        version=$version
+    # find the sb-release section associated with our known release and extract out the list of hyperlinks
+    $splunkVersions = "unknown"
+    $pattern = 'sb-target=\"' + $version + '\" sb-selector="release-version">Splunk Versions:\s(.*?) <\/SB-RELEASE-SELECT>'
+    $m = $element.innerHTML | select-string -pattern $pattern -AllMatches
+    if ($m) {
+
+        $versionLinks = $m.Matches.captures[0].Groups[1].Value
+
+        # extract out the splunk versions from the list of links
+        $m = $versionLinks | select-string -pattern 'version\/([^\"]+)' -AllMatches
+    
+        # transform the array of links to a pipe delimited string
+        [object]$CaptureValues = @()
+        for ($i = 0; $i -le $m.matches.Count -1; $i++)
+        { 
+            $CaptureValue = $m.Matches.Captures[$i].groups[1].Value     
+            $CaptureValues += $CaptureValue
+        }
+        [string]$splunkVersions = $CaptureValues -join "|"
+    } else {
+        $splunkVersions = "none"
+    }
+
+    # EXTRACT OUT THE CIM VERSIONS SUPPORTED BY THIS VERSION OF APP
+
+    # get all container info
+    $element = $WebRequest.AllElements | ?{
+        $_.class -eq "u.container:vspace-xs@* u.container:vpad-md" -and
+        $_.tagname -eq "DIV" -and 
+        $_.innerhtml -match "/apps/#/version/"
+    }
+
+    # find the sb-release section associated with our known release and extract out the list of hyperlinks
+    $CIMVersions = "unknown"
+    $pattern = 'sb-target=\"' + $version + '\" sb-selector="release-version">CIM Versions:\s(.*?) <\/SB-RELEASE-SELECT>'
+    $m = $element.innerHTML | select-string -pattern $pattern -AllMatches
+    if ($m) {
+        $versionLinks = $m.Matches.captures[0].Groups[1].Value
+
+        # extract out the splunk versions from the list of links
+        $m = $versionLinks | select-string -pattern 'cim\/([^\"]+)' -AllMatches
+
+        # transform the array of links to a pipe delimited string
+        [object]$CaptureValues = @()
+        for ($i = 0; $i -le $m.matches.Count -1; $i++)
+        { 
+            $CaptureValue = $m.Matches.Captures[$i].groups[1].Value     
+            $CaptureValues += $CaptureValue
+        }
+        [string]$CIMVersions = $CaptureValues -join "|"
+    } else {
+        $CIMVersions = "none"    
+    }
+
+    # put all this juicy extracted content into an object to return from function
+    $AppInfo = [pscustomobject]@{
+        version = $version
+        filename = $filename
+        splunkVersions = $splunkVersions
+        CIMVersions = $CIMVersions
         id=$appid
     }
+
+    # return new object with desired info
+    return $AppInfo
  
 }
 
-
-<##############################################################################
- MAIN
-##############################################################################>
-
-
-# confirm path of download_path parameter
-if (!(Test-Path -Path $download_path)) {
-    write-host "Path to download folder `"$($download_path)`" not found. Exiting"
-    exit
+# gather cred from credstore
+$mycredfile = "$($SupportFolder)\mycred.xml"
+if (-not(Test-Path -Path $mycredfile)) {
+    Get-Credential -Message "Enter credential for Splunkbase" | Export-Clixml -path $mycredfile
 }
+$cred = Import-Clixml $mycredfile
 
-# handle cred_file_path paramter
-if (!($cred_file_path)) {
-    # prompt for cred if file not supplied
-    $cred = Get-Credential -Message "Enter credential for Splunkbase"
-    if (!($cred)) {
-        write-host "User cancelled credential gathering process. Exiting."
-        exit
-    } 
-} else {
-    # validate path if file supplied as parameter
-    if (Test-Path -Path $cred_file_path) {
-        $cred = Import-Clixml $cred_file_path
-    } else {
-        write-host "Invalid path to cred file - `"$($cred_file_path)`". Exiting."
-        exit
-    }
-}
+$Session = Get-Splunkbase-Session -credFilePath $mycredfile
 
-# handle unattended parameter
-    if ($unattended) {
-        if (!($cred_file_path)) {
-            write-host "Error - cred_file_path parameter must be defined in unattended mode. Exiting."
-            exit
-        }
-    }
-
-# handle app id list parameter (transform to support regex matching)
-if ($app_id_list) {
-    $app_id_list = $app_id_list -Split ','
-    $app_id_list = $app_id_list -join "|"   
-    $app_id_list = "^($($app_id_list)$"
-} else {
-    $app_id_list = ".*"
-}
-
-
-## establish logon session to splunk via okta
-
-$user = $cred.UserName
-$pass = [System.Net.NetworkCredential]::new("", $cred.Password).Password
-
-$BASE_AUTH_URL='https://account.splunk.com/api/v1/okta/auth'
-$Body = @{
-    username = $user
-    password = $pass
-}
-$WebRequest = Invoke-WebRequest $BASE_AUTH_URL -SessionVariable 'Session' -Body $Body -Method 'POST' -UseBasicParsing
-if (!($WebRequest.StatusCode -eq "200")) {
-    write-host "There was a problem authenticating to Splunk.  Exit."
-    exit
-}
-
-## scrape the ssoid from server response and append as cookie to websession object
-$cookie = New-Object System.Net.Cookie    
-$cookie.Name = "SSOSID"
-$cookie.Value = (($WebRequest.Content | ConvertFrom-Json).cookies).ssoid_cookie
-$cookie.Domain = ".splunk.com"
-$session.Cookies.Add($cookie);
-
-<# show cookies 
-$Session.Cookies.GetCookies("https://splunk.com") | select Name, Value, Domain
-#>
 
 # first run just to get the amount of pages to iterate over.
 $url = "https://splunkbase.splunk.com/api/v1/app/?order=latest&limit=1&offset=0"
-$webrequest = invoke-webrequest $url -WebSession $session -UseBasicParsing
-$content = $webrequest.Content | ConvertFrom-Json   
+$response = invoke-webrequest $url -WebSession $session -UseBasicParsing
+$content = $response.Content | ConvertFrom-Json
 
-# gather all of the content available over pages.  
+# gather all of the content available over pages
 $results = @()
 for ($offset = 0; $offset -le $content.total; $offset += 100)
 {
-    $pctComplete = [math]::round(($offset/$content.total)*100,0)
-    Write-Progress -Activity "Splunkbase Manifest Download in Progress" -Status "$pctComplete% Complete:" -PercentComplete $pctComplete;
+    write-debug "Getting next 100 results from offset $($offset) [total=$($content.total)]"
     $url = "https://splunkbase.splunk.com/api/v1/app/?order=latest&limit=100&offset=$($offset)"
-    $webrequest = invoke-webrequest $url -WebSession $Session -UseBasicParsing
-    $content = $webrequest.Content | ConvertFrom-Json
-    
+    $response = invoke-webrequest $url -WebSession $Session -UseBasicParsing
+    $content = $response.Content | ConvertFrom-Json   
     $results += $content.results
-
-    <#
-    # if last record in current page of results is older than age of content we want to download, break out of routine with what we've got.
-    if ($app_age_days) {
-        if ((new-timespan -start ([datetime]($content.results | sort-object -property updated_time | Select-Object -first 1 -ExpandProperty updated_time))).TotalDays -ge $app_age_days) {
-            break
-        }
-    }
-    #>
-
 }
 
+# Get current date/time of our query of splunkbase.  We will commit this as our bookmark if we get to end of script
+$LastQueryDate = Get-Date
 
-# build list of items to download from manifest
-$itemsToDownload = $results | Select-Object -Property updated_time, title, path, archive_status, access, download_count, install_count, uid | Sort-Object -Property updated_time -descending
+# Import the last version of catalog from file if prsent.
+if (test-path -path $catalogfile) { $CachedCatalog = Import-csv -Path $CatalogFile }
 
-# reduce list of items to download to those which match app_id_list parameter
-$itemsToDownload = $itemsToDownload | ?{$_.uid -match $app_id_list}
 
-# reduce list of items to download to those which meet app_age_days parameter constraint
-$itemsToDownload = $itemsToDownload | Where-Object {(new-timespan -start ([datetime]$_.updated_time)).TotalDays -le $app_age_days}
 
-if ($unattended -eq $false) {
-    # prompt user to further reduce list interactively
-    $itemsToDownload = $itemsToDownload | Out-GridView -Title "Select Splunkbase items to download" -PassThru
-}
-
-# download items selected from gridview
 $counter = 0
-$Failures = @()
-foreach ($item in $itemsToDownload) {
+foreach ($item in $results) {
     $counter++
 
-    $pctComplete = [math]::round(($counter/$itemsToDownload.count)*100,0)
-    Write-Progress -Activity "Working on download of `"$($item.title)`"" -Status "$pctComplete% Complete:" -PercentComplete $pctComplete;
+    write-debug "[Item $($counter) of $($results.count)] - Gathering info on `"$($item.title)`" having uid `"$($item.uid)`"."
 
-    # get latest version for app by appid
-    $AppInfo = Get-Splunkbase-AppInfo -appid $item.uid
+    # Check to see splunk base entry is newer than last evaluation
+    if ([datetime]$item.updated_time -gt $BookmarkDate) {
 
-    if ($AppInfo.version -notmatch "^\d") { 
-        write-host "Download url for `"$($item.title)`" doesn't look right. Skipping."  -ForegroundColor Yellow
-        continue
-    }
+        # scrape additional information about app (version, filename, splunk versions supported) from webpage for app on splunkbase
+        $AppInfo = Get-Splunkbase-AppInfo -appid $item.uid
 
-    # build download url from base + appid + app version
-    $Down_URL = "https://splunkbase.splunk.com/app/$($AppInfo.id)/release/$($AppInfo.version)/download/"
+        # add discovered properties to the results array
+        write-debug "`t-Scraped app version `"$($AppInfo.version)`", splunk versions `"$($appinfo.splunkVersions)`", and CIM versions `"$($appinfo.CIMVersions)`" to app record in results array."
+        Add-Member -InputObject $item -MemberType NoteProperty -Force -Name  "version" -Value $AppInfo.version
+        Add-Member -InputObject $item -MemberType NoteProperty -Force -Name  "splunkVersions" -Value $AppInfo.splunkVersions
+        Add-Member -InputObject $item -MemberType NoteProperty -Force -Name  "CIMVersions" -Value $AppInfo.CIMVersions    
 
-    # build the file path to download the item to
-    $filepath = "$download_path\$($appinfo.filename)"
+    } else {
+        # Since entry is not newer, import app info from last update file instead of web (much faster)
+    
+        # pull up app record from object representing previously imported file
+        $Entry = $CachedCatalog | ?{$_.uid -eq $item.uid}
+        write-debug "`t-Imported app version `"$($Entry.version)`", splunk versions `"$($Entry.splunkVersions)`", and CIM versions `"$($Entry.CIMVersions)`" to app record in results array."
 
-    try {
-        $WebRequest = Invoke-WebRequest -Uri $Down_URL -WebSession $Session -OutFile $filepath
-        write-host "`nWebRequest for download of `"$($item.title)`" having access type of `"$($item.access)`" succeeded."
-        write-host "-Download path: `"$($filepath)`""
-    } catch { 
-        write-host "`nWebRequest for download of `"$($item.title)`" having access type of `"$($item.access)`" failed." -ForegroundColor Yellow
-        write-host "-Download url:  `"$($down_url)`"" -ForegroundColor Yellow
-        write-host "-Exception message:  `"$($Error[0].Exception.Message)`"" -ForegroundColor Yellow
-        continue
+        # add version value to member of results array
+        Add-Member -InputObject $item -MemberType NoteProperty -Force -Name  "version" -Value $entry.version
+        Add-Member -InputObject $item -MemberType NoteProperty -Force -Name  "splunkVersions" -Value $entry.splunkVersions
+        Add-Member -InputObject $item -MemberType NoteProperty -Force -Name  "CIMVersions" -Value $entry.CIMVersions
+
     }
 
 }
-write-host ""
-write-host ""
+
+# commit results array appended with additional properties to file
+$results | Export-Csv -Path $CatalogFile -NoTypeInformation
+
+# update bookmark file with date of last run
+Set-Content -Path $BookmarkFile -Value $LastQueryDate.ToString()
+
